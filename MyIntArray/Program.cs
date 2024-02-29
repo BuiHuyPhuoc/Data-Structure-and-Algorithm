85d156ce3650fc3f57b37c728daadf0002195220
BuiHuyPhuoc <128078281+BuiHuyPhuoc@users.noreply.github.com>
2024-02-29 08:31:47 +0700

    add missing file

---
+++ MyIntArray/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MyIntArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 4, 6, 7, 9 };
+            IntArray array = new IntArray(a);
+            Console.WriteLine(array.BinarySearch(4));
+        }
+    }
+}
