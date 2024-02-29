02b91642dc720b71d36160bdc96795be02db7147
BuiHuyPhuoc <128078281+BuiHuyPhuoc@users.noreply.github.com>
2024-02-29 08:15:41 +0700

    init

---
+++ MyIntArray/MyIntArray/MyIntArray.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIntArray
+{
+    public class IntArray
+    {
+        private int[] array;
+
+        public IntArray() 
+        { 
+            array = new int[0];
+        }
+        public IntArray(int[] array) {  this.array = array; }
+
+        public int[] GetArray() { return array;}
+
+        public int BinarySearch(int x)
+        {
+            //x is searched value
+            
+            int left = 0;
+            int right = array.Length-1;
+
+            do
+            {
+                int mid = (left + right) / 2;
+                if (array[mid] == x)
+                {
+                    return x;
+                }
+                else if (array[mid] < x)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            } while (left <= right);
+
+            return -1;
+        }
+
+        public int[] InterchangeSort()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
+        public int[] BoubleSort()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = array.Length - 1; j > i; j--)
+                {
+                    if (array[j] < array[j - 1])
+                    {
+                        int temp = (int)array[j];
+                        array[j] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
+        public int[] SelectionSort()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int vtMin = MinPossition(i);
+                int temp = array[vtMin];
+                array[vtMin] = array[i];
+                array[i] = temp;
+            }
+            return array;
+        }
+
+        private int MinPossition(int i)
+        {
+            int vtMin = i;
+            for (int x = i; x < array.Length; x++)
+            {
+                if (array[vtMin] > array[x])
+                {
+                    vtMin = x;
+                }
+            }
+            return vtMin;
+        }
+
+        public int[] InsertionSort()
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int x = array[i];
+                int pos = i - 1;
+                while (pos >= 0 && array[pos] > x)
+                {
+                    array[pos + 1] = array[pos]; 
+                    pos--;
+                }
+                array[pos+1] = x;
+            }
+            return array;
+        }
+
+        public void QuickSort(int left, int right)
+        {
+            if (left >= right) return;
+
+            int mid = (left + right) / 2;
+            int pivot = array[mid];
+            int i = left;
+            int j = right;
+
+            while (i <= j)
+            {
+                while (array[i] < pivot) i++;
+                while (array[j] > pivot) j--;
+
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            QuickSort(left, j);
+            QuickSort(i, right);
+        }
+
+
+        public int[] ReturnArray()
+        {
+            return array;
+        }
+        public string toString()
+        {
+            string result = "Array: ";
+            foreach (var item in array)
+            {
+
+                result += item.ToString() + " ";
+            }
+            return result;
+        }
+    }
+}
---
+++ MyIntArray/TestProject2/UnitTest1.cs
@@ -0,0 +1,97 @@
+
+using MyIntArray;
+using Newtonsoft.Json.Bson;
+
+namespace TestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestBinarySearch()
+        {
+            int[] a = new int[] { 1, 4, 6, 7, 9 };
+            IntArray array = new IntArray(a);
+            Assert.AreEqual(array.BinarySearch(4), 4);
+        }
+
+        [TestMethod]
+        public void TestInterchangeSort()
+        {
+            int[] a = new int[] { 7, 9, 1, 2, 5 };
+            int[] expResult = new int[] { 1, 2, 5, 7, 9 };
+            IntArray array = new IntArray(a);
+            int[] afterSorted = array.InterchangeSort();
+
+            Assert.AreEqual(expResult.Length, afterSorted.Length);
+
+            for (int i = 0; i < expResult.Length; i++)
+            {
+                Assert.AreEqual(expResult[i], afterSorted[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestBoubleSort()
+        {
+            int[] a = new int[] { 7, 9, 1, 2, 5 };
+            int[] expResult = new int[] { 1, 2, 5, 7, 9 };
+            IntArray array = new IntArray(a);
+            int[] afterSorted = array.BoubleSort();
+
+            Assert.AreEqual(expResult.Length, afterSorted.Length);
+
+            for (int i = 0; i < expResult.Length; i++)
+            {
+                Assert.AreEqual(expResult[i], afterSorted[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestSelectionSort()
+        {
+            int[] a = new int[] { 7, 9, 1, 2, 5 };
+            int[] expResult = new int[] { 1, 2, 5, 7, 9 };
+            IntArray array = new IntArray(a);
+            int[] afterSorted = array.SelectionSort();
+
+            Assert.AreEqual(expResult.Length, afterSorted.Length);
+
+            for (int i = 0; i < expResult.Length; i++)
+            {
+                Assert.AreEqual(expResult[i], afterSorted[i]);
+            }
+        }
+
+        [TestMethod]    
+        public void TestInsertionSort()
+        {
+            int[] a = new int[] { 7, 9, 1, 2, 5 };
+            int[] expResult = new int[] { 1, 2, 5, 7, 9 };
+            IntArray array = new IntArray(a);
+            int[] afterSorted = array.InsertionSort();
+
+            Assert.AreEqual(expResult.Length, afterSorted.Length);
+
+            for (int i = 0; i < expResult.Length; i++)
+            {
+                Assert.AreEqual(expResult[i], afterSorted[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestQuickSort()
+        {
+            int[] a = new int[] { 7, 9, 1, 2, 5 };
+            int[] expResult = new int[] { 1, 2, 5, 7, 9 };
+            IntArray array = new IntArray(a);
+            array.QuickSort(0, a.Length - 1);
+            int[] afterSorted = array.ReturnArray();
+            Assert.AreEqual(expResult.Length, afterSorted.Length);
+            for (int i = 0; i < expResult.Length; i++)
+            {
+                Assert.AreEqual(expResult[i], afterSorted[i]);
+            }
+        }
+    }
+}
